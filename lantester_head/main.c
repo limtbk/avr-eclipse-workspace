4734abaf15f1d2ccc71e15de02eaaf6111553403
lim <ivan.klimchuk@gmail.com>
2014-10-30 00:50:45 +0200

    Update

--- lantester_head/main.c
+++ lantester_head/main.c
@@ -1,22 +1,23 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/pgmspace.h>
 #include "ports.h"
 #include "pcd8544.h"
-#include "hcsr04.h"
 #include "timer1.h"
-#include "usart.h"
 #include "i2c.h"
 #include "ds13xx.h"
 
 void init(void) {
+	DDRC = 0;
+	PORTC = 0;
+	DDRD = 0;
+	PORTD = 0;
 	timer1_init();
 	lcd_init();
-	usart_init();
+	//usart_init();
 
 
 	/*
-	PC6&ADC6 - PC0&ADC0, ADC7&PD4
+	PC6&ADC5 - PC0&ADC0, ADC7&PD3, ADC6&PD4
 
 	PCINT20
 
@@ -25,13 +26,13 @@ void init(void) {
 
 	 */
 	PCICR = (1<<PCIE2) | (1<<PCIE1);
-	PCMSK2 = (1<<PCINT20);
-	PCMSK1 = (1<<PCINT14) | (1<<PCINT13) | (1<<PCINT12) | (1<<PCINT11) | (1<<PCINT10) | (1<<PCINT9) | (1<<PCINT8);
+	PCMSK2 = (1<<PCINT19) | (1<<PCINT20);
+	PCMSK1 = (1<<PCINT13) | (1<<PCINT12) | (1<<PCINT11) | (1<<PCINT10) | (1<<PCINT9) | (1<<PCINT8);
 
-	PCICR = (1<<PCIE1) | (1<<PCIE2);
+//	PCICR = (1<<PCIE1) | (1<<PCIE2);
 
-	PCMSK1 = 0xFF;
-	PCMSK2 = 0xFF;
+//	PCMSK1 = 0xFF;
+//	PCMSK2 = 0xFF;
 
 	sei();
 }
@@ -42,7 +43,7 @@ uint8_t change = 0;
 
 ISR(PCINT1_vect) {
 	INVP(LCD_LED);
-	uint8_t newreg = (PINC & 0b01111111) | ((PIND & (1<<PD4))<<(7-PD4));
+	uint8_t newreg = (PINC & 0b00111111) | ((PIND & ((1<<PD4) | (1<<PD3)))<<(7-PD4));
 	uint8_t changes = (lastreg ^ newreg) & newreg; //detect 0 -> 1
 	if (changes != 0) {
 		lastreg = newreg;
@@ -56,7 +57,7 @@ ISR(PCINT1_vect) {
 
 ISR(PCINT2_vect) {
 	INVP(LCD_LED);
-	uint8_t newreg = (PINC & 0b01111111) | ((PIND & (1<<PD4))<<(7-PD4));
+	uint8_t newreg = (PINC & 0b00111111) | ((PIND & ((1<<PD4) | (1<<PD3)))<<(7-PD4));
 	uint8_t changes = (lastreg ^ newreg) & newreg; //detect 0 -> 1
 	if (changes != 0) {
 		lastreg = newreg;
@@ -75,12 +76,13 @@ void show_connection_bk(void) {
 	lcd_putstr("              ");
 	lcd_putstr("              ");
 	lcd_putstr("12345678  TAIL");
-	lcd_line(CHAR_WIDTH*8, 0, CHAR_WIDTH*8, LCD_HEIGHT-1);
+//	lcd_line(CHAR_WIDTH*8, 0, CHAR_WIDTH*8, LCD_HEIGHT-1);
+	lcd_send_buffer();
 }
 
 void show_connections(uint8_t *connections) {
 	for (uint8_t i = 0; i < 8; i++) {
-		uint8_t c = (connections[i])%8;
+		uint8_t c = (connections[i]);
 		if (c<8) {
 			lcd_line((i)*CHAR_WIDTH+CHAR_WIDTH/2-1, 1*CHAR_HEIGHT+1, (c)*CHAR_WIDTH+CHAR_WIDTH/2-1, 5*CHAR_HEIGHT-1);
 		}
@@ -108,12 +110,12 @@ int main(void)
 
 	uint8_t con_arr[] = {2, 5, 0, 6, 7, 1, 3, 4};
 
-//	lcd_overlay(1);
+	lcd_overlay(0);
 	lcd_optimize(0);
 	lcd_rotate(1);
 
 	while (1) {
-		lcd_clear(0x00);
+		//lcd_clear(0x00);
 		show_connection_bk();
 		show_connections(con_arr);
 		lcd_send_buffer();
