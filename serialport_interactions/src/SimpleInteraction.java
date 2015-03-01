e694af6ec661bcce02423232528df9da928f512d
lim <ivan.klimchuk@gmail.com>
2015-03-02 00:02:01 +0200

    Update

--- serialport_interactions/src/SimpleInteraction.java
+++ serialport_interactions/src/SimpleInteraction.java
@@ -12,81 +12,96 @@ public class SimpleInteraction {
 		
         String[] portNames = SerialPortList.getPortNames("/dev/", Pattern.compile("tty.SLAB_USBtoUART"));
         SerialPort[] serialPorts = new SerialPort[portNames.length];
-        int j = 0;
-        for(int i = 0; i < portNames.length; i++){
+
+        for (int i = 0; i < portNames.length; i++) {
             System.out.println(portNames[i]);
             String portName = portNames[i];
        		SerialPort serialPort = new SerialPort(portName);
-       		int mask = SerialPort.MASK_RXCHAR;
-    		serialPort.setEventsMask(mask);
-//    		serialPort.addEventListener(new SerialPortReader());
-			serialPorts[j] = serialPort;
-			j++;
+       		SerialPortReader portReader = new SerialPortReader();
+       		portReader.index = i;
+       		portReader.serialPort = serialPort;
+       		try {
+				serialPort.openPort();
+	       		int mask = SerialPort.MASK_RXCHAR;
+				serialPort.setEventsMask(mask);
+	    		serialPort.addEventListener(portReader);
+			} catch (SerialPortException e) {
+				e.printStackTrace();
+			}
+			serialPorts[i] = serialPort;
+			i++;
         }
         
         System.out.println(serialPorts);
 
-//        try {
-//            serialPort.openPort();
-//            for (int i = 0; i < 100; i++) {
-//                serialPort.setParams(SerialPort.BAUDRATE_115200, 
-//                        SerialPort.DATABITS_8,
-//                        SerialPort.STOPBITS_1,
-//                        SerialPort.PARITY_NONE);
-//                serialPort.writeBytes("AT\n\r".getBytes());
-//                try {
-//    				Thread.sleep(1);
-//    			} catch (InterruptedException e) {
-//    				e.printStackTrace();
-//    			}
-//                serialPort.setParams(SerialPort.BAUDRATE_57600, 
-//                        SerialPort.DATABITS_8,
-//                        SerialPort.STOPBITS_1,
-//                        SerialPort.PARITY_NONE);
-//                serialPort.writeBytes("AT\n\r".getBytes());
-//                try {
-//    				Thread.sleep(2);
-//    			} catch (InterruptedException e) {
-//    				e.printStackTrace();
-//    			}
-//                serialPort.setParams(SerialPort.BAUDRATE_38400, 
-//                        SerialPort.DATABITS_8,
-//                        SerialPort.STOPBITS_1,
-//                        SerialPort.PARITY_NONE);
-//                serialPort.writeBytes("AT\n\r".getBytes());
-//                try {
-//    				Thread.sleep(4);
-//    			} catch (InterruptedException e) {
-//    				e.printStackTrace();
-//    			}
-//                serialPort.setParams(SerialPort.BAUDRATE_19200, 
-//                        SerialPort.DATABITS_8,
-//                        SerialPort.STOPBITS_1,
-//                        SerialPort.PARITY_NONE);
-//                serialPort.writeBytes("AT\n\r".getBytes());
-//                try {
-//    				Thread.sleep(8);
-//    			} catch (InterruptedException e) {
-//    				e.printStackTrace();
-//    			}
-//                serialPort.setParams(SerialPort.BAUDRATE_9600, 
-//                        SerialPort.DATABITS_8,
-//                        SerialPort.STOPBITS_1,
-//                        SerialPort.PARITY_NONE);
-//                serialPort.writeBytes("AT\n\r".getBytes());
-//                try {
-//    				Thread.sleep(16);
-//    			} catch (InterruptedException e) {
-//    				e.printStackTrace();
-//    			}
-//			}
-//            serialPort.closePort();
-//        }
-//        catch (SerialPortException ex) {
-//            System.out.println(ex);
-//        }		
+        int[] baudRates = {SerialPort.BAUDRATE_115200, SerialPort.BAUDRATE_57600, SerialPort.BAUDRATE_38400, SerialPort.BAUDRATE_19200, SerialPort.BAUDRATE_9600};
+
+        for (int i = 0; i < 160; i++) {
+            for (int baudRate : baudRates) {
+                System.out.println(baudRate);
+                for (SerialPort serialPort : serialPorts) {
+        			try {
+    					serialPort.setParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+    	                serialPort.writeBytes("AT\n\r".getBytes());
+    				} catch (SerialPortException e) {
+    					e.printStackTrace();
+    				}
+        		}
+                
+                try {
+    				Thread.sleep(16);
+    			} catch (InterruptedException e) {
+    				e.printStackTrace();
+    			}
+    		}
+		}
+
+        for (SerialPort serialPort : serialPorts) {
+        	try {
+				serialPort.closePort();
+			} catch (SerialPortException e) {
+				e.printStackTrace();
+			}
+        }
+        
 		System.out.println("Finish");
 
 	}
 
+    static class SerialPortReader implements SerialPortEventListener {
+        public SerialPort serialPort;
+        public int index;
+		public void serialEvent(SerialPortEvent event) {
+            if(event.isRXCHAR()) {
+            	int bytesCount = event.getEventValue(); 
+                if(bytesCount >= 1) {
+                    try {
+                        byte buffer[] = serialPort.readBytes(bytesCount);
+                        String rxString = new String(buffer);
+                        System.out.print(serialPort.getPortName());
+                        System.out.println(rxString);
+                    }
+                    catch (SerialPortException ex) {
+                        System.out.println(ex);
+                    }
+                }
+            }
+//            else if(event.isCTS()){//If CTS line has changed state
+//                if(event.getEventValue() == 1){//If line is ON
+//                    System.out.println("CTS - ON");
+//                }
+//                else {
+//                    System.out.println("CTS - OFF");
+//                }
+//            }
+//            else if(event.isDSR()){///If DSR line has changed state
+//                if(event.getEventValue() == 1){//If line is ON
+//                    System.out.println("DSR - ON");
+//                }
+//                else {
+//                    System.out.println("DSR - OFF");
+//                }
+//            }
+        }
+    }
 }
