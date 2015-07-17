79fb3e0bcb96ddec8cb7c2effc74c5b77e3dc8a2
Ivan Klymchuk <lim.tbk@gmail.com>
2015-07-17 17:33:15 +0300

    Add block read/write

--- common/at24cxx.c
+++ common/at24cxx.c
@@ -37,3 +37,34 @@ uint8_t at24cxx_writebyte(uint16_t addr, uint8_t byte) {
 	return res;
 }
 
+uint8_t at24cxx_read_block (uint16_t addr, void *dst, uint8_t size) {
+	uint8_t res = 0;
+	res = i2c_start(AT24CXX_I2C_ADDRESS << 1);
+	if (res == 0) {
+		res |= i2c_write(HI(addr));
+		res |= i2c_write(LO(addr));
+		res |= i2c_start((AT24CXX_I2C_ADDRESS << 1) | 0x01);
+		uint8_t i;
+		for (i = 0; i < size-1; i++) {
+			((uint8_t *)dst)[i] = i2c_readAck();
+		}
+		((uint8_t *)dst)[i] = i2c_readNak();
+		i2c_stop();
+	}
+	return res;
+}
+
+uint8_t at24cxx_write_block (uint16_t addr, const void *src, uint8_t size) {
+	uint8_t res = 0;
+	res = i2c_start(AT24CXX_I2C_ADDRESS << 1);
+	if (res == 0) {
+		res |= i2c_write(HI(addr));
+		res |= i2c_write(LO(addr));
+		uint8_t i;
+		for (i = 0; i < size; i++) {
+			res |= i2c_write(((uint8_t *)src)[i]);
+		}
+		i2c_stop();
+	}
+	return res;
+}
--- common/at24cxx.h
+++ common/at24cxx.h
@@ -12,5 +12,7 @@
 
 uint8_t at24cxx_readbyte(uint16_t addr);
 uint8_t at24cxx_writebyte(uint16_t addr, uint8_t byte);
+uint8_t at24cxx_read_block (uint16_t addr, void *dst, uint8_t size);
+uint8_t at24cxx_write_block (uint16_t addr, const void *src, uint8_t size);
 
 #endif /* AT24CXX_H_ */
