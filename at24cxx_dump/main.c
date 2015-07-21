b0f410ce250e867466ae1db192e19832ef16018b
Ivan Klymchuk <lim.tbk@gmail.com>
2015-07-21 14:17:29 +0300

    Eeprom dumper

--- at24cxx_dump/main.c
+++ at24cxx_dump/main.c
@@ -28,12 +28,39 @@ int main(void)
 	init();
 	// Dump at24c memory to uart
 
-	usart_printstr("Dump: ");
+	uint8_t ch = usart_getchr();
+	while (1) {
+		if (ch == 'S') {
+			break;
+		}
+	}
+
+	usart_printstr("\n\rDump:\n\r");
 	for (int i = 0; i < 4096; i++) {
 		uint8_t d_i = at24cxx_readbyte(i);
 		usart_printhex(d_i);
 	}
-	usart_printstr(" End");
+	usart_printstr("\n\rErase:\n\r");
+	uint32_t mask = 0xFFFFFFFF;
+	uint32_t check = 0;
+	for (int i = 0; i < 1024; i++) {
+		at24cxx_write_block(i*4, &mask, 4);
+		usart_putchr('.');
+		timer1_delay_ms(5);
+		at24cxx_read_block(i*4, &check, 4);
+		if (check == mask) {
+			usart_putchr('.');
+		} else {
+			usart_putchr('E');
+		}
+		timer1_delay_ms(5);
+	}
+	usart_printstr("\n\rCheck:\n\r");
+	for (int i = 0; i < 4096; i++) {
+		uint8_t d_i = at24cxx_readbyte(i);
+		usart_printhex(d_i);
+	}
+	usart_printstr("\n\rDone\n\r");
 
 	while (1) {
 	}
